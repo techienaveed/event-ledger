318e8b6799a1135d5470d8d6859a11f6d65570f0
techienaveed <techie.naveed@gmail.com>
2026-07-15 04:35:41 -0400

    First cut with pom , docker and main SpringApplication for the 2 new services.

---
+++ event-gateway/src/main/java/com/sample/gateway/EventGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.sample.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class EventGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventGatewayApplication.class, args);
+    }
+
+}
